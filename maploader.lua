c74b9fe474df9a4a7690d21a6f858c3b6612c6f4
AndreFerreira <greiskul@gmail.com>
2009-11-08 18:34:45 -0200

    Adicionei passable = na sintaxe de mapa

--- maploader.lua
+++ maploader.lua
@@ -1,14 +1,14 @@
 function map(t)
 	regmap(t.width,t.height)
 	for i,p in pairs(t.platforms) do
-		regplatform(p[1],p[2],p[3],p[4],p[5])
+		regplatform(p[1],p[2],p[3],p[4],p.passable)
 	end
 	return t
 end
 
 function platform(t)
-	if t[5] == nil then
-		t[5] = true
+	if t.passable == nil then
+		t.passable = true
 	end
 	return t
 end
