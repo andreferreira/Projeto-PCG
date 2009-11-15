37bb721ebbfb258aa8cc370fe0bec6c9008aa294
AndreFerreira <greiskul@gmail.com>
2009-11-15 02:17:28 -0200

    Adicionado funcao auxiliar em lua de numeros aleatorios

--- weapons.lua
+++ weapons.lua
@@ -4,7 +4,11 @@ local cano = {-15,-15+10,-45,-15+10}
 local cano2 = {-15+1,-15+10+1,-45+1,-15+10+1}
 
 math.randomseed( os.time() )
-	
+
+function randomspread(spread)
+	return math.random()*2*spread-spread 
+end
+
 shotgun = Weapon{
 			name = "Shotgun",
 			righthand = {-15,-15},
@@ -18,7 +22,7 @@ shotgun = Weapon{
 					Line(cano2)},
 			whenfire = function (x,y,angle,w)
 						for i = 1,5 do
-							createshot(x,y,angle+math.random()/5-0.1,1000,w)
+							createshot(x,y,angle+randomspread(0.2),1000,w)
 						end
 					   end
 }
